83d1b6cc74c30f653101d58f4121a1331d35f8bc
itsmebabysmiley <snzzare@gmail.com>
2023-05-22 10:50:02 +0700

    updated static path

--- Backend/route/index.js
+++ Backend/route/index.js
@@ -4,29 +4,29 @@ import path, { dirname } from "path";
 
 const router = Router();
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename).replace("/Backend", "");
+const __dirname = dirname(__filename).replace("/Backend/route", "");
 
 
 router.get("/dashboard/grading", (req, res) => {
   const filePath = path.join(
     __dirname,
-    "../Frontend/Gradingpage/Test Grade/index.html"
+    "/Frontend/Gradingpage/Test Grade/index.html"
   );
   res.sendFile(filePath);
 });
 
 router.get("/dashboard", (req, res) => {
-  const filePath = path.join(__dirname, "../Frontend/Dashboard/index.html");
+  const filePath = path.join(__dirname, "/Frontend/Dashboard/index.html");
   res.sendFile(filePath);
 });
 
 router.get("/register", (req, res) => {
-  const filePath = path.join(__dirname, "../Frontend/Signup/index.html");
+  const filePath = path.join(__dirname, "/Frontend/Signup/index.html");
   res.sendFile(filePath);
 });
 
 router.get("/login", (req, res) => {
-  const filePath = path.join(__dirname, "../Frontend/Signin/index.html");
+  const filePath = path.join(__dirname, "/Frontend/Signin/index.html");
   res.sendFile(filePath);
 });
 
@@ -38,7 +38,7 @@ router.get("/dashboard/Suggestions", (req, res) => {
   //Frontend/Suggestions/Test Suggestion/main.html
   const filePath = path.join(
     __dirname,
-    "../Frontend/Suggestions/Test Suggestion/main.html"
+    "/Frontend/Suggestions/Test Suggestion/main.html"
   );
   res.sendFile(filePath);
 });
@@ -47,7 +47,7 @@ router.get("/dashboard/Personalized-jars", (req, res) => {
   //Frontend/9Jars-Moneymanagement/Personalized Jars/main.html
   const filePath = path.join(
     __dirname,
-    "../Frontend/9Jars-Moneymanagement/Personalized Jars/main.html"
+    "/Frontend/9Jars-Moneymanagement/Personalized Jars/main.html"
   );
   res.sendFile(filePath);
 });
@@ -56,33 +56,33 @@ router.get("/dashboard/Fixed-jars", (req, res) => {
   //
   const filePath = path.join(
     __dirname,
-    "../Frontend/9Jars-Moneymanagement/Fixed Jars/test jar/index.html"
+    "/Frontend/9Jars-Moneymanagement/Fixed Jars/test jar/index.html"
   );
   res.sendFile(filePath);
 });
 
 router.get("/dashboard/Calculation", (req, res) => {
   //Frontend/Calculation/index.html
-  const filePath = path.join(__dirname, "../Frontend/Calculation/index.html");
+  const filePath = path.join(__dirname, "/Frontend/Calculation/index.html");
   res.sendFile(filePath);
 });
 
 router.get("/profile", (req, res) => {
   //Frontend/Personal Info/index.html
-  const filePath = path.join(__dirname, "../Frontend/Personal Info/index.html");
+  const filePath = path.join(__dirname, "/Frontend/Personal Info/index.html");
   res.sendFile(filePath);
 });
 
 router.get("/dashboard/test-grading", (req, res) => {
   const filePath = path.join(
     __dirname,
-    "../Frontend/Gradingpage/Test Grade/index.html"
+    "/Frontend/Gradingpage/Test Grade/index.html"
   );
   res.sendFile(filePath);
 });
 
 router.get("/", (req, res) => {
-    const filePath = path.join(__dirname, "../Frontend/Signin/index.html");
+    const filePath = path.join(__dirname, "/Frontend/Signin/index.html");
     res.sendFile(filePath);
 });
 
