cd25905645e7acf340a631b8b1c76585bd829d0f
Chatchai Kraisornsin <51605646+ChatchaiKhaitoon@users.noreply.github.com>
2023-05-24 12:57:19 +0700

    Upadate Calculation

--- Frontend/public/js/calculation-script.js
+++ Frontend/public/js/calculation-script.js
@@ -3,9 +3,10 @@ const calculateBtn = document.querySelector(".calculate-btn");
 calculateBtn.addEventListener("click", () => {
   const expenses = parseFloat(document.getElementById("expenses").value);
   const income = parseFloat(document.getElementById("income").value);
+  const age = parseFloat(document.getElementById("age").value);
 
   // calculate required retirement capital
-  const requiredRetirementCapital = (expenses * 1.8) - income;
+  const requiredRetirementCapital = (expenses - income) * (age*12);
 
   // display result
   document.getElementById("result").innerHTML = "฿" + requiredRetirementCapital.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
